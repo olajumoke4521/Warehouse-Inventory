f55c7093f922752bcc573e5d7953e11460109412
Haonat Araromi <haonatararomi@gmail.com>
2024-12-28 13:14:05 +0100

    initial commit

--- warehouse_inventory/settings.py
+++ warehouse_inventory/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'inventory_db',
         'USER': 'postgres',
-        'PASSWORD': 'Araromi@4521',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
